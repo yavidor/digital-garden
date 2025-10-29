3510ce297dc9d735070d2f94a2b4d486807b7194
yavidor <yonatan@avidor.org>
2025-10-29 07:33:59 +0200

    Quartz sync: Oct 29, 2025, 7:33 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -71,7 +71,7 @@ const config: QuartzConfig = {
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
-      Plugin.Latex({ renderEngine: "mathjax" }),
+      Plugin.Latex({ renderEngine: "katex" }),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
