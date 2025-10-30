7ef3a77122c65667f5325af4e30eb968fb19f8e7
yavidor <yonatan@avidor.org>
2025-10-30 18:40:01 +0200

    Quartz sync: Oct 30, 2025, 6:40 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -10,7 +10,7 @@ export const sharedPageComponents: SharedLayout = {
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
       "Discord Community": "https://discord.gg/cRFFHYye7t",
-      "My GitHub": "https://github.com/yavidor/digital-garden"
+      // "My GitHub": "https://github.com/yavidor/digital-garden"
     },
   }),
 }
