a90a24626787aa96a811be3aba9b9651aa8e9d96
yavidor <yonatan@avidor.org>
2025-11-01 17:40:31 +0200

    Quartz sync: Nov 1, 2025, 5:40 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "he-IL",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
