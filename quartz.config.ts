7c0e5de156044a0720bd83f2aa7d595ee853efa3
yavidor <yonatan@avidor.org>
2025-11-16 15:22:55 +0200

    Quartz sync: Nov 16, 2025, 3:22 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -73,7 +73,7 @@ const config: QuartzConfig = {
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "mathjax" }),
       Plugin.HardLineBreaks(),
-      Plugin.TikzJax(),
+      Plugin.TikzJax({ verbose: true }),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
