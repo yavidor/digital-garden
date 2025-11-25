ba577949e2f3a8deeaf2ed3bc18f555fb53c7ac5
yavidor <yonatan@avidor.org>
2025-11-25 21:21:16 +0200

    Revert "Quartz sync: Nov 25, 2025, 6:30 PM"
    This reverts commit 3e92d7dcb90412a80c597e094cdb69b70f77a927.

--- quartz/plugins/transformers/frontmatter.ts
+++ quartz/plugins/transformers/frontmatter.ts
@@ -87,14 +87,6 @@ export const FrontMatter: QuartzTransformerPlugin<Partial<Options>> = (userOpts)
               allSlugs.push(...file.data.aliases)
             }
 
-            if (data.uid != null && data.uid.toString() !== "") {
-              data.uid = data.uid.toString() as FullSlug
-              const aliases = file.data.aliases ?? []
-              aliases.push(data.uid)
-              file.data.aliases = aliases
-              allSlugs.push(data.uid)
-            }
-
             if (data.permalink != null && data.permalink.toString() !== "") {
               data.permalink = data.permalink.toString() as FullSlug
               const aliases = file.data.aliases ?? []
