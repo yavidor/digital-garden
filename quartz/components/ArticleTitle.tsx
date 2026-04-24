5ea11f569db2dadf534a7e5e556c13c0a8f53a18
yavidor <yonatan@avidor.org>
2026-04-24 17:08:09 +0300

    Quartz sync: Apr 24, 2026, 5:08 PM

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -4,7 +4,7 @@ import { classNames } from "../util/lang"
 const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
     const title = fileData.frontmatter?.title
     if (title) {
-        return <h1 class={classNames(displayClass, "article-title")}>{title} NOder</h1>
+        return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
     } else {
         return null
     }
